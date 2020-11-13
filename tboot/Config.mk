037ede6bcf2bfa6fac8378483289681b7c1e5a58
Lukasz Hawrylko <lukasz.hawrylko@intel.com>
2020-11-13 16:28:35 +0100

    Update changelog, bump version to v1.10.0
    Signed-off-by: Lukasz Hawrylko <lukasz.hawrylko@intel.com>

--- tboot/Config.mk
+++ tboot/Config.mk
@@ -6,8 +6,8 @@
 #
 # tboot-specific build settings
 #
-RELEASEVER  := "1.9.12"
-RELEASETIME := "2020-04-29 15:00 +0200"
+RELEASEVER  := "1.10.0"
+RELEASETIME := "2020-11-13 16:00 +0100"
 ROOTDIR ?= $(CURDIR)/..
 
 # tboot needs too many customized compiler settings to use system CFLAGS,
